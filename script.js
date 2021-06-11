96870f16d7ec5f2a4d3a5409d3e8c3713285b449
Prince Thind <princethind799@gmail.com>
2021-06-11 16:26:41 +0530

    complete-rewrite

--- script.js
+++ script.js
@@ -1,127 +1,109 @@
-const displayDiv = document.querySelector('.display');
-const tempArr = document.querySelectorAll('.key');
-const keyDivs = [...tempArr];
+const numberDivs = document.querySelectorAll("[data-number]");
+const operationDivs = document.querySelectorAll("[data-operation]");
+const enterDiv = document.querySelector("[data-equals]");
+const deleteDiv = document.querySelector("[data-delete]");
+const ACbutton = document.querySelector("[data-all-clear]");
+const upperField = document.querySelector("[data-previous-operand]");
+const currentField = document.querySelector("[data-current-operand]");
 
-let keyPressed = null;
-let displayNumber = 0;
-let currentOperator = null;
-let operatorFlag = false;
-let lastNum = null;
+let displayNumber = "";
+let oldNumber = 0;
+let operator = null;
 
-for (let k of keyDivs)
-{
-    k.addEventListener('click', () => { main(k); })
+
+numberDivs.forEach((button) => {
+  button.addEventListener("click", () => {
+    appendNumber(button.innerText);
+    display();
+  });
+});
+
+operationDivs.forEach((button) => {
+  button.addEventListener("click", () => {
+    chooseOperation(button.innerText);
+    display();
+  });
+});
+
+enterDiv.addEventListener("click", () => {
+  compute();
+  display();
+});
+
+ACbutton.addEventListener("click", () => {
+  reset();
+  display();
+});
+
+deleteDiv.addEventListener("click", () => {
+  backspace();
+  display();
+});
+
+
+
+
+function display() {
+  currentField.textContent = displayNumber;
+  if (operator) {
+    upperField.textContent = oldNumber + " " + operator;
+  } else {
+    upperField.textContent = "";
+  }
 }
 
-function main(k)
-{
-    keyPressed = k.textContent;
-    let type = findType(keyPressed);
-    if (type == 'num')
-    {
-        if (operatorFlag)
-        {
-            lastNum = displayNumber;
-            displayNumber = keyPressed;
-            operatorFlag = false;
-        }
-        else if (displayNumber != 0)
-        {
-            displayNumber = displayNumber + '' + keyPressed;
-        }
-        else
-        {
-            displayNumber = keyPressed;
-        }
-
-    }
-    if (type == 'op')
-    {
-        currentOperator = keyPressed;
-        operatorFlag = true;
-    }
-
-    if (type == 'solve')
-    {
-        displayNumber = operate(currentOperator, lastNum, displayNumber);
-    }
-
-    if (type == 'clear')
-    {
-        keyPressed = null;
-        displayNumber = 0;
-        currentOperator = null;
-        operatorFlag = false;
-        lastNum = null;
-    }
-    display(displayNumber);
+function reset() {
+  displayNumber = "";
+  oldNumber = 0;
+  operator = null;
+  
 }
 
-function findType(key)
-{
-    let res = null;
-    switch (key)
-    {
-        case '0':
-        case '1':
-        case '2':
-        case '3':
-        case '4':
-        case '5':
-        case '6':
-        case '7':
-        case '8':
-        case '9':
-            res = 'num';
-            break;
-        case 'enter':
-            res = 'solve';
-            break;
-        case 'AC':
-            res = 'clear';
-            break;
-        default:
-            res = 'op';
-
-    }
-    return res;
-}
-function operate(op, num1, num2)
-{
-    let res = null;
-    num1 = +num1;
-    num2 = +num2;
-    switch (op)
-    {
-        case '+':
-            res = num1 + num2;
-            break;
-        case '-':
-            res = num1 - num2;
-            break;
-        case '*':
-            res = num1 * num2;
-            break;
-        case '/':
-            res = num1 / num2;
-            break;
-    }
-    return res;
+function backspace() {
+  displayNumber = displayNumber.toString().slice(0, -1);
+  
 }
 
-function display(displayNumber)
-{
-    displayNumber += '';
-
-    let index = displayNumber.indexOf('.');
-    let length = displayNumber.length - index;
-
-    console.log(length);
-    if (length > 4)
-    {
-        displayNumber = +displayNumber;
-        displayNumber = displayNumber.toFixed(4);
-    }
-    displayDiv.textContent = displayNumber;
+function appendNumber(number) {
+  if (number === "." && displayNumber.includes(".")) return;
+  displayNumber = displayNumber.toString() + number.toString();
 }
 
+function chooseOperation(op) {
+  if (displayNumber === "") return;
+  if (oldNumber !== "") {
+    compute();
+  }
+  operator = op;
+  oldNumber = displayNumber;
+  displayNumber = "";
+}
+
+function compute() {
+  let computation;
+  const prev = parseFloat(oldNumber);
+  const current = parseFloat(displayNumber);
+
+  if (isNaN(prev) || isNaN(current)) return;
+
+
+  switch (operator) {
+    case "+":
+      computation = prev + current;
+      break;
+    case "-":
+      computation = prev - current;
+      break;
+    case "*":
+      computation = prev * current;
+      break;
+    case "/":
+      computation = prev / current;
+      break;
+    default:
+      return;
+  }
+  displayNumber = computation;
+  operator = undefined;
+  oldNumber = "";
+}
